22b4d6523a3181719710062cdee38b73c2eacf5c
Hao-OCT <patrick.wang1025@gmail.com>
2019-09-16 23:43:31 +1000

    auth...validation...

--- app.js
+++ app.js
@@ -2,16 +2,19 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 // ===================
 //    Models & DB
 // ===================
-mongoose.connect("mongodb://localhost/cc", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+const url = "mongodb://dbUser:aiculus2019S2@ds235418.mlab.com:35418/cloudcomputing";
+mongoose.connect(url, { useNewUrlParser: true, useFindAndModify: false , useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB...'))
+    .catch(err => console.error('Could not connect to MongoDB...'));
 
+app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cookieParser());
 app.set("view engine", "ejs");
 // ====================
 //       Routers
@@ -21,8 +24,6 @@ const home = require("./routes/home");
 
 app.use("/home", home);
 app.use("/users", user);
-app.get("/login", (req, res) => res.render("login"));
-app.get("/register", (req, res) => res.render("register"));
 app.get("/", (req, res) => res.redirect("/home"));
 app.get("*", (req, res) => res.sendStatus(404));
 
--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const jwt = require("jsonwebtoken");
 const withAuth = require("../middleware/auth");
+const { User, validateRegister, validateChange } = require("../models/user");
 
 //TODO: For real application, should keep this secret an actual secret
 //using environment variables or some other method and make sure you
@@ -12,7 +13,64 @@ const secret = "This is a secret String";
 //password is correct. If the password is correct, we will issue
 //a signed token to the requester.
 
+router.post("/login", function(req, res) {
+    const { email, password } = req.body;
+    User.findOne({ email }, function(err, user) {
+      if (err) {
+        console.log(err);
+        res.status(500).json({
+          error: "Internal error please try again"
+        });
+      } else if (!user) {
+        res.status(401).json({
+          error: "Incorrect username or password"
+        });
+      } else {
+        user.isCorrectPassword(password, function(err, same) {
+          if (err) {
+            res.status(500).json({
+              error: "Internal error please try again"
+            });
+          } else if (!same) {
+            res.status(401).json({
+              error: "Incorrect username or password"
+            });
+          } else {
+            //Issue token
+            const payload = { email };
+            const token = jwt.sign(payload, secret, {
+              expiresIn: "1h"
+            });
+            res.cookie("token", token, { httpOnly: true }).sendStatus(200);
+            //res.send(token);
+          }
+        });
+      }
+    });
+  });
+
+  router.post("/register", async (req, res) => {
+    try {
+      const { error } = validateRegister(req.body);
+      if (error) return res.status(400).send(error.details[0].message);
+  
+      const user = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: req.body.password
+      });
+      await user.save();
+      res.status(200).send(user);
+    } catch (ex) {
+      console.log(ex.errors);
+      res.status(500).send(ex.message);
+    }
+  });
 
 router.get("/", (req, res) => res.send("This is the User page"));
+
+router.get("/login", (req, res) => res.render("login"));
+router.get("/register", (req, res) => res.render("register"));
 
 module.exports = router;
