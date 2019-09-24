7486fe173b5aee74c702f5be99895a3966f41400
Hao-OCT <patrick.wang1025@gmail.com>
2019-09-24 20:17:05 +1000

    Latest
    Latest version of backend

--- app.js
+++ app.js
@@ -2,29 +2,58 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-const cookieParser = require('cookie-parser');
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
+// var unirest = require("unirest");
+
+// var req = unirest(
+//   "GET",
+//   "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2019-10-01"
+// );
+
+// req.query({
+//   inboundpartialdate: "2019-12-01"
+// });
+
+// req.headers({
+//   "x-rapidapi-host": "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
+//   "x-rapidapi-key": "8654aad1e3mshbe551f028e8ff9ap1bad9cjsnf616b195d23f"
+// });
+
+// req.end(function(res) {
+//   if (res.error) throw new Error(res.error);
+
+//   console.log(res.body);
+// });
 // ===================
 //    Models & DB
 // ===================
-const url = "mongodb://dbUser:aiculus2019S2@ds235418.mlab.com:35418/cloudcomputing";
-mongoose.connect(url, { useNewUrlParser: true, useFindAndModify: false , useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch(err => console.error('Could not connect to MongoDB...'));
+const url =
+  "mongodb://dbUser:aiculus2019S2@ds235418.mlab.com:35418/cloudcomputing";
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error("Could not connect to MongoDB..."));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.set("view engine", "ejs");
+app.use(cors());
 // ====================
 //       Routers
 // ====================
 const user = require("./routes/user");
 const home = require("./routes/home");
 
-app.use("/home", home);
-app.use("/users", user);
-app.get("/", (req, res) => res.redirect("/home"));
+app.use("/api/home", home);
+app.use("/api/users", user);
+app.get("/", (req, res) => res.redirect("/api/home"));
 app.get("*", (req, res) => res.sendStatus(404));
 
 const port = process.env.PORT || 3000;
--- routes/user.js
+++ routes/user.js
@@ -14,63 +14,65 @@ const secret = "This is a secret String";
 //a signed token to the requester.
 
 router.post("/login", function(req, res) {
-    const { email, password } = req.body;
-    User.findOne({ email }, function(err, user) {
-      if (err) {
-        console.log(err);
-        res.status(500).json({
-          error: "Internal error please try again"
-        });
-      } else if (!user) {
-        res.status(401).json({
-          error: "Incorrect username or password"
-        });
-      } else {
-        user.isCorrectPassword(password, function(err, same) {
-          if (err) {
-            res.status(500).json({
-              error: "Internal error please try again"
-            });
-          } else if (!same) {
-            res.status(401).json({
-              error: "Incorrect username or password"
-            });
-          } else {
-            //Issue token
-            const payload = { email };
-            const token = jwt.sign(payload, secret, {
-              expiresIn: "1h"
-            });
-            res.cookie("token", token, { httpOnly: true }).sendStatus(200);
-            //res.send(token);
-          }
-        });
-      }
-    });
-  });
-
-  router.post("/register", async (req, res) => {
-    try {
-      const { error } = validateRegister(req.body);
-      if (error) return res.status(400).send(error.details[0].message);
-  
-      const user = new User({
-        firstName: req.body.firstName,
-        lastName: req.body.lastName,
-        email: req.body.email,
-        password: req.body.password
+  const { email, password } = req.body;
+  User.findOne({ email }, function(err, user) {
+    if (err) {
+      console.log(err);
+      res.status(500).json({
+        error: "Internal error please try again"
       });
-      await user.save();
-      res.status(200).send(user);
-    } catch (ex) {
-      console.log(ex.errors);
-      res.status(500).send(ex.message);
+    } else if (!user) {
+      res.status(401).json({
+        error: "Incorrect username or password"
+      });
+    } else {
+      user.isCorrectPassword(password, function(err, same) {
+        if (err) {
+          res.status(500).json({
+            error: "Internal error please try again"
+          });
+        } else if (!same) {
+          res.status(401).json({
+            error: "Incorrect username or password"
+          });
+        } else {
+          //Issue token
+          const { firstName, lastName, _id } = user;
+          const payload = { email, firstName, lastName, _id };
+          const token = jwt.sign(payload, secret, {
+            expiresIn: "2h"
+          });
+          //res.cookie("token", token, { httpOnly: true }).sendStatus(200);
+          res.send(token);
+        }
+      });
     }
   });
+});
 
-router.get("/", (req, res) => res.send("This is the User page"));
+router.post("/register", async (req, res) => {
+  try {
+    const { error } = validateRegister(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
 
-router.get("/login", (req, res) => res.render("login"));
-router.get("/register", (req, res) => res.render("register"));
+    const user = new User({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password
+    });
+    await user.save();
+    //Issue token
+    const { firstName, lastName, email, _id } = user;
+    const payload = { email, firstName, lastName, _id };
+    const token = jwt.sign(payload, secret, {
+      expiresIn: "2h"
+    });
+    res.send(token);
+  } catch (ex) {
+    console.log(ex.errors);
+    res.status(500).send(ex.message);
+  }
+});
 
 module.exports = router;
